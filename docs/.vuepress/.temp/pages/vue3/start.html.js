9b262278ebd5dc97b1278aa8aadc0673f3751eac
jgoriginal <932274204@qq.com>
2022-07-29 00:44:31 +0800

    update

--- docs/.vuepress/.temp/pages/vue3/start.html.js
+++ docs/.vuepress/.temp/pages/vue3/start.html.js
@@ -165,16 +165,3 @@ export const data = {
   },
   "filePathRelative": "vue3/start.md"
 }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
